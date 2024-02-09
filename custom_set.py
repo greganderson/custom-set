05fa7819b556d3d4d1f6262b48ab8a35dcc950fb
Greg Anderson <greg.anderson.cs@gmail.com>
2024-02-09 10:15:50 -0700

    Stub out the CustomSet class
    This is my first commit for the CustomSet assignment in the best class
    in the world. Way better than Utah Tech, at least. And University of
    Utah.

---
+++ custom_set.py
@@ -0,0 +1,16 @@
+class CustomSet:
+
+    def __init__(self):
+        pass
+
+    def add(self, item) -> None:
+        pass
+
+    def remove(self, item) -> None:
+        pass
+    
+    def as_list(self) -> list:
+        pass
+
+    def clear(self) -> None:
+        pass
