55ee20da5849fc6836924f04a09732f79c09af06
Greg Anderson <greg.anderson.cs@gmail.com>
2024-02-09 10:33:08 -0700

    Add docstrings

--- custom_set.py
+++ custom_set.py
@@ -1,16 +1,21 @@
 class CustomSet:
+    """ This is a small, simple implementation of a set """
 
     def __init__(self):
         pass
 
     def add(self, item) -> None:
+        """ Takes an item and adds it to the set """
         pass
 
     def remove(self, item) -> None:
+        """ Takes an item and removes it to the set """
         pass
     
     def as_list(self) -> list:
+        """ Returns a copy of the set as a list """
         pass
 
     def clear(self) -> None:
+        """ Clears all the items from the set """
         pass
